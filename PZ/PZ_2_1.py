e7a17fad9ad459f6d478b4739bc107fa224639e0
Artem-Maistrenko <rtrt123789456@gmail.com>
2024-11-01 18:43:50 +0300

    Update PZ_2_1.py

--- PZ/PZ_2_1.py
+++ PZ/PZ_2_1.py
@@ -1,43 +1,15 @@
 def transform_number(number):
-    """
-    Функция принимает трехзначное число,
-    зачёркивает первую цифру и приписывает её справа.
-
-    :param number: трёхзначное целое число
-    :return: преобразованное число
-    """
-    # Преобразуем число в строку для работы с цифрами
-    number_str = str(number)
-
-    # Проверяем, что число является трехзначным
-    if len(number_str) != 3 or not number_str.isdigit():
+    if not (100 <= number < 1000):
         raise ValueError("Ввод должен быть трёхзначным числом.")
-
-    # Извлекаем первую цифру и оставшиеся две
-    first_digit = number_str[0]
-    remaining_digits = number_str[1:]
-
-    # Формируем новое число
-    new_number_str = remaining_digits + first_digit
-
-    return int(new_number_str)
-
+    return (number % 100) * 10 + number // 100
 
 def main():
     try:
-        # Запрашиваем у пользователя ввод трёхзначного числа
         user_input = int(input("Введите трехзначное число: "))
-
-        # Вызываем функцию для преобразования числа
         result = transform_number(user_input)
-
-        # Выводим результат
         print(f"Преобразованное число: {result}")
-
     except ValueError as e:
         print(f"Ошибка: {e}")
 
-
 if __name__ == "__main__":
     main()
-
