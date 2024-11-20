96ccf0017aa3e9b1cbd10609ea80385b9dab7490
Artem <rtrt123789456@gmail.com>
2024-11-20 23:57:33 +0300

    .

---
+++ PZ/PZ_4_1.py
@@ -0,0 +1,23 @@
+
+
+# Дано вещественное число X и целое число N (> 0).
+# Найти значение выражения 1 + X + X^2/(2!) + ... + X^N/(N!) (N! = 12 ...N).
+# Полученное число является приближенным значением функции exp в точке X.
+
+import math
+
+def approximate_exp(X, N):
+    try:
+        if N <= 0: raise ValueError("N должно быть больше 0")
+        result, term = 1.0, 1.0
+        for i in range(1, N + 1):
+            term *= X / i
+            result += term
+        return result
+    except Exception as e:
+        return f"Ошибка: {e}"
+
+X = float(input("Введите вещественное число X: "))
+N = int(input("Введите целое число N (> 0): "))
+
+print(approximate_exp(X, N))
