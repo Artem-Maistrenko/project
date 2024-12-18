982d76b2a30634fac6e6a80153b19753c06fd3f3
Artem <rtrt123789456@gmail.com>
2024-12-18 09:11:51 +0300

    update

--- PZ/PZ_3/PZ_3_2.py
+++ PZ/PZ_3/PZ_3_2.py
@@ -1,12 +1,20 @@
 # Даны три числа. Найти среднее из них (то есть число, расположенное между
 # наименьшим и наибольшим).
 
-num1 = float(input("Введите первое число: "))
-num2 = float(input("Введите второе число: "))
-num3 = float(input("Введите третье число: "))
+# Обработка ввода чисел
+try:
+    num1 = float(input("Введите первое число: "))
+    num2 = float(input("Введите второе число: "))
+    num3 = float(input("Введите третье число: "))
 
-smallest = min(num1, num2, num3)
-largest = max(num1, num2, num3)
-average = (num1 + num2 + num3) - smallest - largest
+    # Вычисление среднего числа
+    smallest = min(num1, num2, num3)
+    largest = max(num1, num2, num3)
+    average = (num1 + num2 + num3) - smallest - largest
 
-print(f"число, расположенное между наименьшим и наибольшим: {average}")
+    # Вывод результата
+    print(f"Число, расположенное между наименьшим и наибольшим: {average}")
+
+# Обработка ошибок ввода
+except ValueError as e:
+    print("Ошибка")
