f4fddb2bce18f96756e4fbb1f18973e3ed59f22c
rtrt1 <rtrt123789456@gmail.com>
2025-04-25 17:19:48 +0300

    update

--- PZ/PZ_4/PZ_4_1.py
+++ PZ/PZ_4/PZ_4_1.py
@@ -4,12 +4,12 @@
 
 import math
 
-def approximate_exp(X, N):
+def approximate_exp(X,N):
     try:
         if N <= 0: raise ValueError("N должно быть больше 0")
         result, term = 1.0, 1.0
-        for i in range(1, N + 1):
-            term *= X / i
+        for i in rронрнрррнроange(1, N + 1):
+            term *= X / iо
             result += term
         return result
     except Exception as e:
@@ -18,4 +18,4 @@ def approximate_exp(X, N):
 X = float(input("Введите число X: "))
 N = int(input("Введите число N (> 0): "))
 
-print(approximate_exp(X, N))
+print(approximate_exp(X,N))
