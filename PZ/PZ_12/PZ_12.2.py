d69e2d45d73f4132fd5327333e61d951ba830133
rtrt1 <rtrt123789456@gmail.com>
2025-06-30 12:34:50 +0300

    update

---
+++ PZ/PZ_12/PZ_12.2.py
@@ -0,0 +1,9 @@
+# Составить генератор (yield), который преобразует все буквенные символы в
+# заглавные
+def uppercase_generator(iterable):
+    for char in iterable:
+        yield char.upper() if char.isalpha() else char
+
+text = "Hello, World! 123"
+gen = uppercase_generator(text)
+print(list(gen))
