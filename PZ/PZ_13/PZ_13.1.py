dd251d7834abbc10a688b6be053942db78622f44
Artem-Maistrenko <rtrt123789456@gmail.com>
2025-06-30 12:20:07 +0300

    Update PZ_13.1.py

--- PZ/PZ_13/PZ_13.1.py
+++ PZ/PZ_13/PZ_13.1.py
@@ -1,8 +1,8 @@
+# в двумерном списке элементы кратные 3 увеличить в 3 раза
 def multiply_threes(nested_list):
     return [list(map(lambda x: x * 3 if x % 3 == 0 else x, sublist)) 
             for sublist in nested_list]
 
-# Пример использования:
 original_list = [
     [1, 2, 3],
     [4, 5, 6],
